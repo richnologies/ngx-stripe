59c5a03c1b39abc0af379a3d0dcb57eb8ac3c7b4
Ricardo Sánchez <me@ricardosanchez.dev>
2022-05-15 06:24:23 +0000

    feat(docs): better sticky aside scroll behavior

--- projects/ngx-stripe-docs/src/app/docs-elements/section/section.component.ts
+++ projects/ngx-stripe-docs/src/app/docs-elements/section/section.component.ts
@@ -17,18 +17,16 @@ import { NgStrSubheaderComponent } from '../subheader/subheader.component';
 @Component({
   selector: 'ngstr-section',
   template: `
-    <div class="flex-1 justify-center relative z-0 flex h-screen overflow-hidden">
-      <main
-        #mainContentContainer
-        class="flex-1 m-auto py-10 max-w-screen-md h-full w-11/12 my-0 mx-1 overflow-y-auto max-w-2xl"
-      >
+    <div #sectionContainer class="flex-1 justify-center relative z-0 flex h-screen overflow-y-auto">
+      <main class="flex-1 m-auto py-10 max-w-screen-md h-full w-11/12 my-0 mx-1 max-w-2xl">
         <div class="mt-6 prose prose-indigo text-gray-500 mx-auto">
           <ng-content select="[ngStrSectionMain]"></ng-content>
           <div class="!mt-8"></div>
           <ngstr-section-navigator></ngstr-section-navigator>
+          <div class="!mt-8"></div>
         </div>
       </main>
-      <aside class="hidden xl:block xl:pl-8 w-1/3 overflow-y-auto max-w-sm" *ngIf="aside">
+      <aside class="hidden xl:block xl:pl-8 w-1/3 max-w-sm overflow-y-auto sticky top-0" *ngIf="aside">
         <div class="space-y-5 mr-8 mt-16">
           <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <ngstr-copy-link></ngstr-copy-link>
@@ -45,7 +43,7 @@ import { NgStrSubheaderComponent } from '../subheader/subheader.component';
   `
 })
 export class NgStrSectionComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('mainContentContainer') main: ElementRef;
+  @ViewChild('sectionContainer') section: ElementRef;
   @ContentChildren(NgStrSubheaderComponent) subheaders = new QueryList<NgStrSubheaderComponent>();
 
   @Input() aside = true;
@@ -58,11 +56,11 @@ export class NgStrSectionComponent implements AfterViewInit, OnDestroy {
   constructor(private router: Router) {}
 
   ngAfterViewInit() {
-    merge(fromEvent(this.main.nativeElement, 'scroll').pipe(debounceTime(250)), fromEvent(window, 'resize'))
+    merge(fromEvent(this.section.nativeElement, 'scroll').pipe(debounceTime(250)), fromEvent(window, 'resize'))
       .pipe(takeUntil(this.onDestroy))
       .subscribe(() => {
-        const mainClientReact = this.main.nativeElement.getBoundingClientRect();
-        const trigger = mainClientReact && mainClientReact.height ? Math.floor(mainClientReact.height / 6) : 150;
+        const sectionClientReact = this.section.nativeElement.getBoundingClientRect();
+        const trigger = sectionClientReact && sectionClientReact.height ? Math.floor(sectionClientReact.height / 6) : 150;
 
         this.activeSection = this.contents
           .filter((content) => {
